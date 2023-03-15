e28d418bd1b7df05821f8378eca6b0d92bfbd480
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-03-15 17:26:29 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [headingText, updateHT] = useState("");
+
+  // name = ""
+  // event.target.value = "pallab"
+  // setName = "pallab" tarmane now name = "pallab"
+
+  // input e text likhchi jokhun tokhun this fn is running
+  function handleChange(event) {
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+  // button e click korle this fn is running
+  // now headingtext = ""
+  // now updateHT = name i.e. updateHt = pallab
+  // so headingtext = pallab
+  function hanleClick() {
+    updateHT(name);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {headingText}</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
+      <button onClick={hanleClick}>Submit</button>
     </div>
   );
 }
